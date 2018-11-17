eba874f21dc1556848ae74697ff459a60a3d31da
Mateusz <dev.mateuszkaleta@gmail.com>
2018-11-17 13:57:14 +0100

    Add standardization

--- hakaton/model/image.py
+++ hakaton/model/image.py
@@ -1,15 +1,19 @@
-from skimage.io import imread, imsave
 import os
 from hakaton.model.transformation import ImageTransformation
+from matplotlib import pyplot as plt
+import scipy.ndimage
 
 
 class Image:
 
     def __init__(self, file_path: str, grayscale: bool=False):
-        self._data = imread(file_path, as_gray=grayscale)
+        self._data = scipy.ndimage.imread(file_path, mode='L' if grayscale else None, flatten=grayscale)
 
     def apply_transformation(self, transformation: ImageTransformation):
         self._data = transformation.apply(self._data)
+
+    def apply_lambda_transformation(self, transformation_func):
+        self._data = transformation_func(self._data)
 
     @property
     def ndarray(self):
@@ -33,4 +37,4 @@ class Image:
 
     def save(self, path: str):
         os.makedirs(os.path.dirname(path), exist_ok=True)
-        imsave(path, self._data)
+        plt.imsave(path, self._data)
